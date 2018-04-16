a3dbf5b60ab7afdb89bf99dd3edff598a6eb608d
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-16 15:02:22 +0300

    Lesson 8.'Components Within Components'

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+Vue.component('task-list', {
+    template: `
+    <ul>
+        <task v-for="task in tasks" :key="task.id">{{ task.description }}</task>
+    </ul>
+    `,
+
+    data() {
+        return {
+            tasks: [
+                { description: 'Task 1', completed: true },
+                { description: 'Task 2', completed: false },
+                { description: 'Task 3', completed: true },
+                { description: 'Task 4', completed: false },
+                { description: 'Task 5', completed: false },
+                { description: 'Task 6', completed: true }
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
