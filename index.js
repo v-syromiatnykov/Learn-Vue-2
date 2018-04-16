eea86313a8b241a123b18acc0ad03464b3855372
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-16 15:29:55 +0300

    Lesson 9.'Practical Component Exercise #1: Message'

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-Vue.component('task-list', {
+Vue.component('message', {
+    props: ['title', 'body'],
+
     template: `
-    <ul>
-        <task v-for="task in tasks" :key="task.id">{{ task.description }}</task>
-    </ul>
+        <article class="message" v-show="isVisible">
+        <div class="message-header">
+            <p>{{ title }}</p>
+            <button class="delete" aria-label="delete" @click="hideModal"></button>
+        </div>
+        <div class="message-body">{{ body }}</div>
+    </article>
     `,
 
     data() {
         return {
-            tasks: [
-                { description: 'Task 1', completed: true },
-                { description: 'Task 2', completed: false },
-                { description: 'Task 3', completed: true },
-                { description: 'Task 4', completed: false },
-                { description: 'Task 5', completed: false },
-                { description: 'Task 6', completed: true }
-            ]
+            isVisible: true
+        }
+    },
+
+    methods: {
+        hideModal() {
+            this.isVisible = false;
         }
     }
-});
-
-Vue.component('task', {
-    template: '<li><slot></slot></li>'
 });
 
 new Vue({
