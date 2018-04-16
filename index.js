5c99ae2c675b5caa1b45cc3e6025f1706f330613
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-16 16:05:02 +0300

    Lesson 10.'Practical Component Exercise #2: Modal'

--- index.js
+++ index.js
@@ -24,6 +24,27 @@ Vue.component('message', {
     }
 });
 
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <slot></slot>
+            </div>
+        </div>
+        <button class="modal-close is-large"
+         aria-label="close"
+         @click="$emit('close')"></button>
+    </div>
+    `,
+
+});
+
 new Vue({
-    el: '#root'
+    el: '#root',
+
+    data: {
+        showModal: false
+    }
 });
