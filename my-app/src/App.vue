cceb021a981aac749bd4435a0568e483507490e7
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-17 13:09:54 +0300

    Lesson 17.'Hot Module Replacement'

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,20 +1,23 @@
 <template>
     <div id="app">
         <message>hello there</message>
+
+        <counter></counter>
     </div>
 </template>
 
 <script>
-import Message from './components/Message.vue';
-    
-export default {
-    name: 'app',
-    components: { Message },
-    data () {
-        return {
+    import Message from './components/Message.vue';
+    import Counter from './components/Counter.vue';
+
+    export default {
+        name: 'app',
+        components: { Message, Counter },
+        data () {
+            return {
+            }
         }
     }
-}
 </script>
 
 <style>
---
+++ my-app/src/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>The count: {{ count }}</h3>
+
+        <button @click="count++">Increment</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return { count: 0 };
+        }
+    }
+</script>
