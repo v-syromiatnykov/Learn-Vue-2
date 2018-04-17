a917027f811ad9b57f3ee511a68034457144cdec
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-17 14:04:44 +0300

    Lesson 18.'Vue Ajax Requests With Axios'

---
+++ vue-app/public/js/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#root',
+
+    data: {
+        skills: []
+    },
+
+    mounted() {
+        axios.get('/skills').then(response => this.skills = response.data);
+    }
+});
---
+++ vue-app/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div id="root">
+            <ul>
+                <li v-for="skill in skills" v-text="skill"></li>
+            </ul>
+        </div>
+
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
