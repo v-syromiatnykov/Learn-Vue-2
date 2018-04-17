371e9236dfe593bc6f2bcaa272d230a64318313a
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-17 12:51:59 +0300

    Lesson 16.'Webpack and vue-cli'

---
+++ my-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <message>hello there</message>
+    </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+    
+export default {
+    name: 'app',
+    components: { Message },
+    data () {
+        return {
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
